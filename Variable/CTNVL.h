cd0880f6638c5ad567f546a2b0922ff94e261450
kouhei.takemoto <take64@citrus.tk>
2017-12-18 02:27:59 +0900

    [リファクタリング][CODEBEAT]CTNVL

--- Variable/CTNVL.h
+++ Variable/CTNVL.h
@@ -3,7 +3,7 @@
 //  CitrusTouch3
 //
 //  Created by take64 on 2017/03/31.
-//  Copyright © 2017年 citrus.tk. All rights reserved.
+//  Copyright © 2017 citrus.tk. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
@@ -11,9 +11,9 @@
 @interface CTNVL : NSObject
 
 // NVL関数
-+ (id) compare:(id)compare replace:(id)replace;
++ (id)compare:(id)compare replace:(id)replace;
 
 // NVL2関数
-+ (id) compare:(id)compare value1:(id)value1 value2:(id)value2;
++ (id)compare:(id)compare value1:(id)value1 value2:(id)value2;
 
 @end
