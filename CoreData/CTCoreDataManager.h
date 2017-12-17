715f501af23b26bb6338e0dc64a8e762388a09ed
kouhei.takemoto <take64@citrus.tk>
2017-12-18 02:44:46 +0900

    [リファクタリング][CODEBEAT]CTCoreDataManager

--- CoreData/CTCoreDataManager.h
+++ CoreData/CTCoreDataManager.h
@@ -3,7 +3,7 @@
 //  CitrusTouch3
 //
 //  Created by take64 on 2017/03/27.
-//  Copyright © 2017年 citrus.tk. All rights reserved.
+//  Copyright © 2017 citrus.tk. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
@@ -14,22 +14,24 @@
 {
     // コンテクスト
     NSManagedObjectContext *context;
-    
+
     // モデル
     NSManagedObjectModel *model;
-    
+
     // ストア
     NSPersistentStoreCoordinator *coordinator;
-    
+
     // モデル名
     NSString *modelName;
-    
+
     // モデル名拡張子
     NSString *modelExtension;
-    
+
     // ストアパス
     NSString *storePath;
 }
+
+
 
 //
 // property
@@ -50,12 +52,6 @@
 // 初期化
 - (id) initWithStorePath:(NSString *)storePath modelName:(NSString *)modelName modelExtension:(NSString *)modelExtension;
 
-
-
-//
-// caller method
-//
-
 // コンテクスト
 - (NSManagedObjectContext *)managedObjectContext:(NSThread *)thread;
 
@@ -65,32 +61,13 @@
 // コンテクスト(カレントスレッド)
 - (NSManagedObjectContext *)managedObjectContextForCurrentThread;
 
-//// モデル
-//- (NSManagedObjectModel *)managedObjectModel;
-//
-//// ストア
-//- (NSPersistentStoreCoordinator *)persistentStoreCoordinator;
-
-//// 保存
-//- (BOOL) saveContext;
-//
-//// 保存&マージ
-//- (void) saveWithMergeObject:(NSManagedObject *)managedObject;
-
 // 保存＆mainThread保存
 - (void) saveComplete;
-
-//// 削除
-//- (BOOL) deleteWithSave:(NSManagedObject *)managedObject;
 
 // ロールバック
 - (void) rollbackContext;
 
-//// 保存通知
-//- (void) managedObjectContextDidSave:(NSNotification *)notification;
-
 // データオブジェクト生成
 - (NSManagedObject *)newObjectWithEntityName:(NSString *)entityName;
-
 
 @end
--- CoreData/CTCoreDataManager.m
+++ CoreData/CTCoreDataManager.m
@@ -3,7 +3,7 @@
 //  CitrusTouch3
 //
 //  Created by take64 on 2017/03/27.
-//  Copyright © 2017年 citrus.tk. All rights reserved.
+//  Copyright © 2017 citrus.tk. All rights reserved.
 //
 
 #import "CTCoreDataManager.h"
@@ -25,6 +25,7 @@ static const NSString *NSManagedObjectContextThreadKey = @"NSManagedObjectContex
 @synthesize storePath;
 
 
+
 #pragma mark - method
 //
 // method
@@ -34,7 +35,7 @@ static const NSString *NSManagedObjectContextThreadKey = @"NSManagedObjectContex
 - (id)initWithStorePath:(NSString *)_storePath modelName:(NSString *)_modelName modelExtension:(NSString *)_modelExtension
 {
     self = [super init];
-    if(self)
+    if (self)
     {
         // 初期設定
         [self setModelName      :_modelName];
@@ -44,23 +45,15 @@ static const NSString *NSManagedObjectContextThreadKey = @"NSManagedObjectContex
     return self;
 }
 
-
-
-
-#pragma mark - caller method
-//
-// caller method
-//
-
 // コンテクスト
 - (NSManagedObjectContext *)managedObjectContext:(NSThread *)thread
 {
     // 対象スレッドからコンテキスト取得
     NSMutableDictionary *threadDictionary = [thread threadDictionary];
     NSManagedObjectContext *_context = [threadDictionary objectForKey:NSManagedObjectContextThreadKey];
-    
+
     // 取得できない場合
-    if(_context == nil)
+    if (_context == nil)
     {
         // 生成
         _context = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
@@ -71,60 +64,52 @@ static const NSString *NSManagedObjectContextThreadKey = @"NSManagedObjectContex
         // マージポリシー
         [_context setMergePolicy:NSMergeByPropertyObjectTrumpMergePolicy];
     }
-    
+
     return _context;
 }
+
 // コンテクスト(メインスレッド)
 - (NSManagedObjectContext *)managedObjectContextForMainThread
 {
     return [self managedObjectContext:[NSThread mainThread]];
 }
+
 // コンテクスト(カレントスレッド)
 - (NSManagedObjectContext *)managedObjectContextForCurrentThread
 {
     return [self managedObjectContext:[NSThread currentThread]];
 }
+
 // モデル
 - (NSManagedObjectModel *)managedObjectModel
 {
-    if ([self model] != nil)
+    if ([self model] == nil)
     {
-        return [self model];
+        NSURL *modelURL = [[NSBundle mainBundle] URLForResource:[self modelName] withExtension:[self modelExtension]];
+        [self setModel:[[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL]];
     }
-    
-    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:[self modelName] withExtension:[self modelExtension]];
-    [self setModel:[[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL]];
-    
     return [self model];
 }
 
 // ストア
 - (NSPersistentStoreCoordinator *)persistentStoreCoordinator
 {
-    if ([self coordinator] != nil)
+    if ([self coordinator] == nil)
     {
-        return [self coordinator];
+        NSURL *storeURL = [[[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject] URLByAppendingPathComponent:[self storePath]];
+        NSDictionary *options = @{NSSQLitePragmasOption:@{@"journal_mode":@"DELETE"},
+                                  NSMigratePersistentStoresAutomaticallyOption:@YES,
+                                  NSInferMappingModelAutomaticallyOption:@YES,
+                                  };
+        NSError *error = nil;
+        [self setCoordinator:[[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]]];
+        if (![[self coordinator] addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:options error:&error])
+        {
+            NSLog(@"Unresolved error %@", error);
+            NSLog(@"%@", [error userInfo]);
+            abort();
+        }
     }
-    
-    NSURL *storeURL = [[[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject] URLByAppendingPathComponent:[self storePath]];
-    
-    
-    NSDictionary *options = @{NSSQLitePragmasOption:@{@"journal_mode":@"DELETE"},
-                              NSMigratePersistentStoresAutomaticallyOption:@YES,
-                              NSInferMappingModelAutomaticallyOption:@YES,
-                              };
-    
-    NSError *error = nil;
-    [self setCoordinator:
-     [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]]
-     ];
-    if (![[self coordinator] addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:options error:&error])
-    {
-        NSLog(@"Unresolved error %@", error);
-        NSLog(@"%@", [error userInfo]);
-        abort();
-    }
-    
     return [self coordinator];
 }
 // 保存
@@ -132,83 +117,74 @@ static const NSString *NSManagedObjectContextThreadKey = @"NSManagedObjectContex
 {
     NSManagedObjectContext *_context = [self managedObjectContextForCurrentThread];
     BOOL isMainThread = [[NSThread currentThread] isMainThread];
-    
+
     // 返却値
     BOOL result;
-    
+
     // メインスレッドでない場合は通知する
-    if(isMainThread == NO)
+    if (isMainThread == NO)
     {
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(managedObjectContextDidSave:)
-                                                     name:NSManagedObjectContextDidSaveNotification
-                                                   object:_context];
-        
-        
-        // 保存処理
-        NSError *error;
-        result = [_context save:&error];
-        if(error != nil)
-        {
-            NSLog(@"error : %@", error);
-        }
-        
+        // 通知登録
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(managedObjectContextDidSave:) name:NSManagedObjectContextDidSaveNotification object:_context];
+    }
+    // 保存処理
+    NSError *error;
+    result = [_context save:&error];
+    if (error != nil)
+    {
+        NSLog(@"error : %@", error);
+    }
+    // メインスレッドでない場合は通知する
+    if (isMainThread == NO)
+    {
         // 通知削除
-        [[NSNotificationCenter defaultCenter] removeObserver:self
-                                                        name:NSManagedObjectContextDidSaveNotification
-                                                      object:_context];
+        [[NSNotificationCenter defaultCenter] removeObserver:self name:NSManagedObjectContextDidSaveNotification object:_context];
     }
-    else
-    {
-        // 保存処理
-        result = [_context save:nil];
-    }
-    
     return result;
 }
+
 // 保存&マージ
 - (void)saveWithMergeObject:(NSManagedObject *)managedObject
 {
     // 保存
     [self saveContext];
-    
     // マージ
     [[self managedObjectContextForCurrentThread] refreshObject:managedObject mergeChanges:YES];
 }
+
 // 削除
 - (BOOL)deleteWithSave:(NSManagedObject *)managedObject
 {
     // 削除
     [[self managedObjectContextForCurrentThread] deleteObject:managedObject];
-    
     // 保存
     return [self saveContext];
 }
+
 // 保存＆mainThread保存
 - (void)saveComplete
 {
     [self saveContext];
-    
     dispatch_async(dispatch_get_main_queue(), ^{
-        
         [self saveContext];
     });
 }
+
 // ロールバック
 - (void)rollbackContext
 {
-    NSManagedObjectContext *_context = [self managedObjectContextForCurrentThread];
-    [_context rollback];
+    [[self managedObjectContextForCurrentThread] rollback];
 }
+
 // 保存通知
 - (void)managedObjectContextDidSave:(NSNotification *)notification
 {
     NSManagedObjectContext *_context = [self managedObjectContextForMainThread];
-    
     [_context performSelectorOnMainThread:@selector(mergeChangesFromContextDidSaveNotification:)
                                withObject:notification
                             waitUntilDone:YES];
 }
+
 // データオブジェクト生成
 - (NSManagedObject *)newObjectWithEntityName:(NSString *)entityName
 {
