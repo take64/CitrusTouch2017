21c3507d04329d291aa8d2d98e12baf2283d7ec5
kouhei.takemoto <take64@citrus.tk>
2017-12-17 19:26:02 +0900

    [リファクタリング][CODEBEAT]CTTableCellTextView

--- UI/Table/TableCell/CTTableCellTextView.m
+++ UI/Table/TableCell/CTTableCellTextView.m
@@ -3,37 +3,40 @@
 //  CitrusTouch3
 //
 //  Created by take64 on 2017/04/22.
-//  Copyright © 2017年 citrus.tk. All rights reserved.
+//  Copyright © 2017 citrus.tk. All rights reserved.
 //
 
 #import "CTTableCellTextView.h"
 
 @implementation CTTableCellTextView
 
+
+
 //
 // synthesize
 //
 @synthesize textView;
 @synthesize didEndEditingBlock;
-//@synthesize didEndEditingTarget;
-//@synthesize didEndEditingSelector;
 
+
+
+#pragma mark - extends
+//
+// extends
+//
 
 - (void)layoutSubviews
 {
     [super layoutSubviews];
-    
+
     // 未レイアウトの場合
-    if([self isSubLayouted] == NO)
+    if ([self isSubLayouted] == NO)
     {
         [[self textView] setFrame:[self contentFrame]];
-        
-        
         // レイアウト済み
         [self setSubLayouted:YES];
     }
 }
-
 
 
 
@@ -45,31 +48,23 @@
 // 編集開始前
 - (BOOL)textViewShouldBeginEditing:(UITextView *)textView
 {
-//    [self activate];
     return YES;
 }
 //- (BOOL)textViewShouldEndEditing:(UITextView *)textView;
 //- (void)textViewDidBeginEditing:(UITextView *)textView;
+
 // 編集終了後
 - (void)textViewDidEndEditing:(UITextView *)textView
 {
-//    [self deactivate];
-    
     // 編集後処理が登録されていて、呼べれば呼ぶ
-    if(self.didEndEditingBlock != nil)
+    if (self.didEndEditingBlock != nil)
     {
         self.didEndEditingBlock();
     }
-//    if([[self didEndEditingTarget] respondsToSelector:[self didEndEditingSelector]] == YES)
-//    {
-//        [[self didEndEditingTarget] performSelector:[self didEndEditingSelector]];
-//    }
-    
 }
 //- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text;
 //- (void)textViewDidChange:(UITextView *)textView;
 //- (void)textViewDidChangeSelection:(UITextView *)textView;
-
 
 
 
@@ -88,11 +83,11 @@
 - (id) initWithPrefix:(NSString *)prefixString suffix:(NSString *)suffixString reuseIdentifier:(NSString *)reuseIdentifier
 {
     self = [super initWithPrefix:prefixString suffix:suffixString reuseIdentifier:reuseIdentifier];
-    if(self)
+    if (self)
     {
         // part
         CTTextView *_textView;
-        
+
         // テキストフィールド
         _textView = [[CTTextView alloc] initWithFrame:CGRectZero];
         [_textView setFont:[UIFont systemFontOfSize:16.0]];
@@ -105,36 +100,12 @@
                                               UIViewAutoresizingFlexibleWidth       |
                                               UIViewAutoresizingFlexibleHeight)];
         [_textView setDelegate:self];
-//        [self setResponder:[self textView]];
         [_textView setBackgroundColor:[UIColor clearColor]];
         [[self contentView] addSubview:_textView];
         [self setTextView:_textView];
-        
-        
-        
-        
-//        [self setTextView:[[CTTextView alloc] initWithFrame:CGRectZero]];
-//        [[self textView] setFont:[UIFont systemFontOfSize:16.0]];
-//        [[self textView] setAutocapitalizationType:UITextAutocapitalizationTypeNone];
-//        [[self textView] setReturnKeyType:UIReturnKeyDone];
-//        [[self textView] setAutoresizingMask:(UIViewAutoresizingFlexibleLeftMargin |
-//                                              UIViewAutoresizingFlexibleRightMargin |
-//                                              UIViewAutoresizingFlexibleTopMargin |
-//                                              UIViewAutoresizingFlexibleBottomMargin |
-//                                              UIViewAutoresizingFlexibleWidth	|
-//                                              UIViewAutoresizingFlexibleHeight)];
-//        [[self textView] setDelegate:self];
-//        [self setResponder:[self textView]];
-//        [[self contentView] addSubview:[self textView]];
-//        
-//        [[self textView] setBackgroundColor:[UIColor clearColor]];
-        
-        
+
         // セル選択
         [self setSelectionStyle:UITableViewCellSelectionStyleNone];
-        
-//        // ツールバー(配置)
-//        [[self textView] setInputAccessoryView:[self toolbar]];
     }
     return self;
 }
@@ -142,9 +113,8 @@
 // 初期化
 - (id)initWithPrefix:(NSString *)prefixString content:(NSString *)textString suffix:(NSString *)suffixString reuseIdentifier:(NSString *)reuseIdentifier
 {
-    
     self = [self initWithPrefix:prefixString suffix:suffixString reuseIdentifier:reuseIdentifier];
-    if(self)
+    if (self)
     {
         [[self textView] setText:textString];
     }
@@ -156,17 +126,11 @@
 {
     return [[self textView] text];
 }
+
 // テキスト設定
 - (void)setContentText:(NSString *)stringValue
 {
     [[self textView] setText:stringValue];
 }
-
-//// テキスト編集完了時
-//- (void)setDidEndEditingTarget:(id)target selector:(SEL)selector
-//{
-//    [self setDidEndEditingTarget:target];
-//    [self setDidEndEditingSelector:selector];
-//}
 
 @end
